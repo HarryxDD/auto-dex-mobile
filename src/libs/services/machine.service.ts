edfe010395b2e62975805c336a30514a0da27e3e
systemis <cuthinh2002@icloud.com>
2024-06-30 01:35:06 +0700

    chore: implement user activites

--- src/libs/services/machine.service.ts
+++ src/libs/services/machine.service.ts
@@ -4,6 +4,11 @@ import { ChainID, PoolEntity, PoolStatus, UserToken } from "@/libs/entities/pool
 import qs from "qs";
 
 export class MachineService {
+  /**
+   * Create a new machine pool off-chain
+   * @param walletAddress 
+   * @returns 
+   */
   async createEmptyMachinePoolOffChain(walletAddress: string) {
     const response = await axiosInstance.post<any>(
       `/api/pool/avaxc/${walletAddress}`, {}
@@ -12,6 +17,11 @@ export class MachineService {
     return response.data as unknown as PoolEntity;
   }
 
+  /**
+   * Sync machine pool
+   * @param machineId 
+   * @returns 
+   */
   async syncMachine(machineId: string) {
     return axiosInstance.post(
       `/api/pool/evm/${machineId}/sync`, {},
@@ -20,6 +30,11 @@ export class MachineService {
     );
   }
 
+  /**
+   * Get quote
+   * @param getQuoteDto 
+   * @returns 
+   */
   async getQuote(getQuoteDto: GetQuoteDto) {
     const response = await axiosInstance.get<any>(
       `/api/metadata/market/quote`,
@@ -31,6 +46,11 @@ export class MachineService {
     return response.data;
   }
 
+  /**
+   * Get machine list
+   * @param ownerAddress
+   * @param statuses
+   */
   async getMachineList(
     ownerAddress: string,
     statuses: PoolStatus[]
@@ -51,6 +71,10 @@ export class MachineService {
     return response.data;
   }
 
+  /**
+   * Get machine list by owner address
+   * @param ownerAddress
+   */
   async syncWalletMachines(walletAddress: string) {
     return axiosInstance.post(
       `/api/pool/user/evm/${walletAddress}/sync`, {},
@@ -62,11 +86,19 @@ export class MachineService {
     );
   }
 
+  /**
+   * Get machine list by owner address
+   * @param ownerAddress
+   */
   async getMachine(machineId: string) {
     const response = await axiosInstance.get<PoolEntity>(`/api/pool/${machineId}`);
     return response.data;
   }
 
+  /**
+   * Get machine list by owner address
+   * @param ownerAddress
+   */
   async getMachineActivities(machineId: string) {
     const response = await axiosInstance.get<MachineActivity[]>(
       `/api/pool/${machineId}/activities`
@@ -81,12 +113,29 @@ export class MachineService {
     return response.data;
   }
 
+  /**
+   * Get portfolio pnl
+   * @param walletAddress
+   * @returns
+   */
   async getPortfolioPnl(walletAddress: string) {
     const response = await axiosInstance.get<Array<{
       ownerAddress: string;
       totalROIValueInUSD: number;
     }>>(
       `/api/portfolio/${walletAddress}/pnl?chainId=avaxc`
+    );
+    return response.data;
+  }
+
+  /**
+   * Get user activities
+   * @param walletAddress
+   * @returns
+   */
+  async getUserActivities(walletAddress: string) {
+    const response = await axiosInstance.get<MachineActivity[]>(
+      `/api/pool/user-activities?ownerAddress=${walletAddress}`
     );
     return response.data;
   }
